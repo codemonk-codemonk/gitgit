34210d4a659a90cc8c4703ea56293556d36d607d
unknown <Abhishek Jadhav>
2019-11-11 15:02:50 +0530

    Admin dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -89,18 +89,60 @@
                         <li><a href="{{ url('/aboutus') }}">About us</a></li>
                         <li><a href="{{ url('/contactus') }}">Contact us</a></li>
                     @else
+
+
+                        <?php
+
+                          $curr  = auth()->user();
+                          if($curr->usertype == "admin")
+                          {
+                            ?>
+
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <ul class="dropdown-menu" role="menu">
+                                    <li><a href="{{ url('/home') }}"><i class="fa fa-btn fa-sign-out"></i>Profile</a></li>
+                                    <li><a href="{{ url('/policy') }}"><i class="fa fa-btn fa-sign-out"></i>Policy</a></li>
+                                      <li><a href="{{ url('/dashboard') }}"><i class="fa fa-btn fa-sign-out"></i>Dashboard</a></li>
+                                    <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                                </ul>
+                            </li>
+
+
+
+                        <?php
+                      }else{
+                        ?>
+
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                 {{ Auth::user()->name }} <span class="caret"></span>
                             </a>
 
                             <ul class="dropdown-menu" role="menu">
-                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Profile</a></li>
+                                <li><a href="{{ url('/home') }}"><i class="fa fa-btn fa-sign-out"></i>Profile</a></li>
                                 <li><a href="{{ url('/policy') }}"><i class="fa fa-btn fa-sign-out"></i>Policy</a></li>
                                 <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
                             </ul>
                         </li>
+
+
+                        <?php
+                      }
+
+
+
+                         ?>
+
+
                     @endif
+
+
+
+
 
                 </ul>
             </div>
